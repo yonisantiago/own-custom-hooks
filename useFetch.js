49a382ece5505e7f25e0571e0fc54f00049f3106
Yoni Silvestre <yonisantiagosg@gmail.com>
2022-03-04 22:12:26 -0500

    First comit

---
+++ useFetch.js
@@ -0,0 +1,52 @@
+import {useState, useEffect, useRef} from 'react';
+
+export const useFetch = (url) => {
+  
+    const isMounted = useRef(true);
+  
+    //importing breaking bad API https://www.breakingbadapi.com/api
+    const [state, setState] = useState({data: null, loading: true, error: null})
+
+    useEffect(() => {
+    
+        return () => {
+            isMounted.current = false;
+        }
+
+    }, [])
+    
+
+
+    useEffect(() => {
+        
+        setState({data: null, loading: true, error:null});
+
+        fetch(url)
+        .then(resp => resp.json() )
+        .then(data => {
+            
+
+              if (isMounted.current) {
+                
+                setState({
+                    loading: false,
+                    error:null,
+                    data
+                });
+
+              }  
+       
+        })
+        .catch(()=> {
+            setState({
+                data: null, 
+                loading: false, 
+                error: 'No se pudo cargar la info'
+            })
+        })
+    
+      
+    }, [url])
+    
+    return state;
+}
